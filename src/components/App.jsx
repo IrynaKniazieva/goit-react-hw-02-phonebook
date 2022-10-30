35371cb9f5c790e76d7558167b326d28b53b46cf
IrynaKniazieva <gleb0812@gmail.com>
2022-10-31 01:56:39 +0200

    in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Form from './Form/Form'
+import ContactForm from "./ContactForm/ContactForm"
 import ContactList from "./ContactList/ContactList"
 import ContactItem from "./ContactItem/ContactItem"
 import Filter from "./Filter/Filter"
@@ -53,10 +53,10 @@ class App extends React.Component {
 
     return (
       <div>
-        <h1>Phonebook</h1>
-        <Form onAddContact={this.addContact} />
+        <h1 className={}>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} />
 
-        <h2>Contacts</h2>
+        <h2 className={}>Contacts</h2>
         <Filter filter={filter} onChange={this.changeFilter} />
         <ContactList>
           <ContactItem
